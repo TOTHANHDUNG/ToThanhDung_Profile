f63caf008347550afa36604149fbf58b736e3cf5
TOTHANHDUNG <tothanhdung2k2@gmail.com>
2022-08-27 23:10:19 +0700

    Initial commit

---
+++ app/src/main/java/com/example/tthnhdng_2050531200120_profile/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.tthnhdng_2050531200120_profile;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
